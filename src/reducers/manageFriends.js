dd4280f0ff26f9d0f86afb76550ce7021d2606f3
sjgarza22 <sjgarza22@gmail.com>
2021-06-02 19:06:35 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+    switch(action.type) {
+        case 'ADD_FRIEND':
+            return { friends: [...state.friends,
+                {
+                name: action.friend.name,
+                hometown: action.friend.hometown,
+                id: action.friend.id
+            }]}
+        case 'REMOVE_FRIEND':
+            const newFriends = [];
+            state.friends.forEach(friend => {
+                if(friend.id !== action.id){
+                    newFriends.push(friend)
+                }
+            })
+            return { friends: newFriends }
+        default:
+            return state
+    }
 }
